e288d58d76305996755d87a9a6621404a3a5355d
bonz88 <kenan.sabljakovic@gmail.com>
2024-02-23 12:53:45 +0100

    added option to select due date and due time

--- src/app/components/TaskForm.tsx
+++ src/app/components/TaskForm.tsx
@@ -4,11 +4,14 @@ import Link from "next/link";
 import TaskInput from "./TaskInput";
 import TaskLevels from "./TaskLevels";
 import { BackIcon } from "../icons/BackIcon";
+import TaskDueDateTime from "./TaskDueDateTime";
 
 export default function TaskForm({ label }: { label: string }) {
   const [value, setValue] = useState<string>("");
   const [priority, setPriority] = useState<number>(0);
   const [complexity, setComplexity] = useState<number>(0);
+  const [dueDate, setDueDate] = useState<string>("");
+  const [dueTime, setDueTime] = useState<string>("");
   const [taskNameError, setTaskNameError] = useState<string>("");
 
   const handlePriority = (level: number) => {
@@ -17,6 +20,16 @@ export default function TaskForm({ label }: { label: string }) {
 
   const handleComplexity = (level: number) => {
     setComplexity(level);
+  };
+
+  const handleDueDate = (date: string) => {
+    console.log(date);
+    setDueDate(date);
+  };
+
+  const handleDueTime = (time: string) => {
+    console.log(time);
+    setDueTime(time);
   };
 
   console.log("priority: ", priority);
@@ -44,6 +57,14 @@ export default function TaskForm({ label }: { label: string }) {
             handleComplexity={handleComplexity}
             priority={priority}
             complexity={complexity}
+          />
+        </div>
+        <div className="mt-[30px]">
+          <TaskDueDateTime
+            dueDate={dueDate}
+            dueTime={dueTime}
+            handleDueDate={handleDueDate}
+            handleDueTime={handleDueTime}
           />
         </div>
       </div>
